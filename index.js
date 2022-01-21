41f271ef8d84f10e8db62070c14b3309f5ea42a8
Nastasi Davydov <nastasidavydov@gmail.com>
2022-01-21 22:54:41 +0000

    Used classes to implement banking app

--- index.js
+++ index.js
@@ -1,29 +1,64 @@
-let balance = 500.00;
-
-class Withdrawal {
-
-  constructor(amount) {
+class Transaction {
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+    if (!this.isAllowed()){
+      return false;
+    }
+    this.time = new Date();
+    this.account.addTransaction(this);
+    return true;
+  }
+}
+
+
+////// WITHDRAWAL Class ////////
+class Withdrawal extends Transaction{
+
+  get value() {
+    return -this.amount;
+  }
+
+  isAllowed(){
+    return (this.account.balance - this.amount >= 0);
   }
 
 }
 
+////// DEPOSIT Class ////////
+class Deposit extends Transaction{
 
+  get value() {
+    return this.amount;
+  }
 
+  isAllowed() {
+    return true;
+  }
+}
 
-// DRIVER CODE BELOW
-// We use the code below to "drive" the application logic above and make sure it's working as expected
+////// ACCOUNT Class ////////
 
-t1 = new Withdrawal(50.25);
-t1.commit();
-console.log('Transaction 1:', t1);
+class Account {
+  constructor(username){
+    this.username = username;
 
-t2 = new Withdrawal(9.99);
-t2.commit();
-console.log('Transaction 2:', t2);
+    this.transactions = [];
+  }
 
-console.log('Balance:', balance);
+  get balance() {
+    let balance = 0;
+    for(let transaction of this.transactions) {
+      balance += transaction.value;
+    }
+    return balance;
+  }
+
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+  }
+}
+
